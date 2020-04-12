556cbf7b2a218d04c8cd0ada85c5582100898e79
renata-dimoska <renata.dimoska@yahoo.com>
2020-04-12 17:48:57 +0200

    taxReturn method finished

--- Item.java
+++ Item.java
@@ -7,10 +7,35 @@ class Item {
           A=18%;
 
 	//TODO constructor
+        Item(int id, String name, double price){
+         this.id = id;
+         this.name = name;
+         this.price = price;
+        }
 
 	//TODO setters and getters
+        public void setId(int id){
+          this.id = id;
+         }
+        public void setName(String name){
+          this.name = name;
+         }
+        public void setPrice(double price){
+          this.price = price;
+         }
+
+        public int getId(){
+         return id;
+        }
+        public String getName(){
+         return name;
+        }
+        public double getPrice(){
+         return price;
+        }
 
 	double taxReturn () {
 		//TODO
+           return price*A*15%;
 	}
 }
